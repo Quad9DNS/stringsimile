6ba94a18137fafda0f55b46b7468398f81f9a087
Ensar Sarajčić <dev@ensarsarajcic.com>
2025-04-17 16:43:30 +0200

    Add basic service binary

--- bin/stringsimile-service/src/main.rs
+++ bin/stringsimile-service/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use tokio::io::{self, AsyncBufReadExt, BufReader};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let reader_task = tokio::spawn(async move {
+        let stdin = io::stdin();
+        let reader = BufReader::new(stdin);
+        let mut lines = reader.lines();
+
+        while let Some(line) = lines.next_line().await.expect("reading failed") {
+            println!("length = {}", line.len())
+        }
+    });
+    reader_task.await.map_err(Box::new)?;
+    Ok(())
 }
